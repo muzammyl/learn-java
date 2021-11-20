2191d8bfca4e5f29d9991c763fecfbb9618aff08
muzammyl <muzammil_75@outlook.com>
2021-11-20 20:36:25 +0500

    added comments

---
+++ src/comment/comment.java
@@ -0,0 +1,23 @@
+package src.comment;
+
+/* remember: comments are ignored by the compiler */
+
+// single-line comment
+
+/*
+    multi-line comment
+    we can add more than one line in this type of comment
+*/
+
+/**
+ * Documentation comment
+ * it allows you to embed information
+ * related to your program into program itself
+ */
+
+
+/**
+ * Documentation comment example
+ * @author Muhammad Muzammyl
+ * @version 1.0
+ */
