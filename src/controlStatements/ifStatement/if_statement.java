6b9505827ff22c8f191567a3d930d06120f70f09
muzammyl <muzammil_75@outlook.com>
2021-11-20 20:36:41 +0500

    added if in control*

---
+++ src/controlStatements/ifStatement/if_statement.java
@@ -0,0 +1,10 @@
+package src.controlStatements.ifStatement;
+
+//simple example of if statement
+class if_statement {
+    public static void main(String[] args) {
+        if (1 > 0) {
+            System.out.print("1 is greater than 0");
+        }
+    }
+}
