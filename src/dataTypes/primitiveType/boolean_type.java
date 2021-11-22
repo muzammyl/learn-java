2f6fe990d103ecb78b0fdb6442ab5e699e1b61dc
muzammyl <muzammil_75@outlook.com>
2021-11-22 22:24:21 +0500

    added boolean*

---
+++ src/dataTypes/primitiveType/boolean_type.java
@@ -0,0 +1,15 @@
+package src.dataTypes.primitiveType;
+
+class boolean_type {
+    public static void main(String[] args) {
+        boolean b;
+        b = true;
+        if(b) {
+            System.out.print("b is " + b);
+        }
+        b = false;
+        if(b) {
+            System.out.print("b is " + b);
+        }
+    }
+}
