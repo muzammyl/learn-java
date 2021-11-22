6f36d415a55cb399112eae4fa479ff75bf4a45bc
muzammyl <muzammil_75@outlook.com>
2021-11-22 22:24:05 +0500

    added forloop*

---
+++ src/controlStatements/forStatement/for_loop.java
@@ -0,0 +1,10 @@
+package src.controlStatements.forStatement;
+
+class for_loop {
+    public static void main(String[] args) {
+        int x;
+        for(x = 0; x < 5; x++) { // these are code blocks {}
+            System.out.println("x is " + x);
+        }
+    }
+}
