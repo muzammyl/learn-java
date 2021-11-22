8d6cbdc8bc38290df1dcebf58b99f6e23d38b0a8
muzammyl <muzammil_75@outlook.com>
2021-11-22 22:25:09 +0500

    added longtype*

---
+++ src/dataTypes/primitiveType/long_type.java
@@ -0,0 +1,9 @@
+package src.dataTypes.primitiveType;
+
+class long_type {
+    public static void main(String[] args) {
+        long l;
+        l = 24 * 60 * 60 * 1118000;
+        System.out.println("value of l is " + l);
+    }
+}
