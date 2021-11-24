0c8a981bef991a17fc5459debc840920a70c2eed
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:16:10 +0500

    added char in dataType*

---
+++ src/dataTypes/primitiveType/char_type.java
@@ -0,0 +1,10 @@
+package src.dataTypes.primitiveType;
+
+class char_type {
+    public static void main(String[] args) {
+        char c1, c2;
+        c1 = 88;
+        c2 = 'Y';
+        System.out.println("c1 and c2 are " + c1 + " " + c2);
+    }
+}
