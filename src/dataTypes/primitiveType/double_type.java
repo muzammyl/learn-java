95ee7661a54032740b95fad2f68cac9f39699646
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:16:37 +0500

    added double in dataType*

---
+++ src/dataTypes/primitiveType/double_type.java
@@ -0,0 +1,11 @@
+package src.dataTypes.primitiveType;
+
+class double_type {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 13.5;
+        pi = 3.1416;
+        a = pi * r *r;
+        System.out.println("Area of circle is " + a);
+    }
+}
