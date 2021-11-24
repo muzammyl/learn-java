ba381a602cba63db994fc8f4f0ace60c66f5be64
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:18:41 +0500

    added short in dataType*

---
+++ src/dataTypes/primitiveType/short_type.java
@@ -0,0 +1,10 @@
+package src.dataTypes.primitiveType;
+
+class short_type {
+    public static void main(String[] args) {
+        short s, t;
+        s = 786;
+        t = 789;
+        System.out.println("short sum(s+t) is " + (s + t));
+    }
+}
