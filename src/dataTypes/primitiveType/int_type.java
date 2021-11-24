a25ab5d9581c05497a0b16a0f0ccb3e702fa6ed2
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:18:08 +0500

    added int in dataType*

---
+++ src/dataTypes/primitiveType/int_type.java
@@ -0,0 +1,11 @@
+package src.dataTypes.primitiveType;
+
+class int_type {
+    public static void main(String[] args) {
+        int i, j, k;
+        i = 43003;
+        j = 105;
+        k = i + j;
+        System.out.println("value of int(k) is " + k);
+    }
+}
