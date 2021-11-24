62a63042fbb90d2c3b909543a5d9bd4beb498c06
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:15:38 +0500

    added byte in dataType*

---
+++ src/dataTypes/primitiveType/byte_type.java
@@ -0,0 +1,10 @@
+package src.dataTypes.primitiveType;
+
+class byte_type {
+    public static void main(String[] args) {
+        byte b, c;
+        b = 101;
+        c = 27;
+        System.out.println("byte sum(b+c) is " + (b+c));
+    }
+}
