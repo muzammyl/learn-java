77d750ad13891acede9785d5e41b80a3b4c9d13e
muzammyl <muzammil_75@outlook.com>
2021-11-24 22:16:59 +0500

    added float in dataType*

---
+++ src/dataTypes/primitiveType/float_type.java
@@ -0,0 +1,10 @@
+package src.dataTypes.primitiveType;
+
+class float_type {
+    public static void main(String[] args) {
+        float f, g;
+        f = 10.8f;
+        g = 10f;
+        System.out.println("float sum(f+g) is " + (f+g));
+    }
+}
