1f4a473a41afea03964b36db399db7f74da56e2e
muzammyl <muzammil_75@outlook.com>
2021-12-26 14:03:29 +0500

    added explicit* in type casting

---
+++ src/typeCasting/explicit.java
@@ -0,0 +1,15 @@
+public class explicit {
+    public static void main(String[] args) {
+        byte b;
+        int i = 356;
+        double d = 215.786;
+
+        System.out.println("Conversion of int to byte.");
+        b = (byte) i;
+        System.out.println("Now int (i) is a byte type " + i + " to " + b + "\n");
+
+        System.out.println("Conversion of double to int type.");
+        i = (int) d;
+        System.out.println("Now double (d) is an int type " + d + " to " + i);
+    }
+}
