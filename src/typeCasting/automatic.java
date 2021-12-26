0577c3329a24b2c967e91272e9a91848b51a6493
muzammyl <muzammil_75@outlook.com>
2021-12-26 14:03:03 +0500

    added autocast* in typecasting

---
+++ src/typeCasting/automatic.java
@@ -0,0 +1,7 @@
+public class automatic {
+    public static void main(String[] args) {
+        byte b = 56;
+        int i = b;
+        System.out.println("value of byte converted into int type that is " + i);
+    }
+}
