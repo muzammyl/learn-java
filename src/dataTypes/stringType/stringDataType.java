97b09f5dafad0040b5cb1047b19c02cb611dc5d2
muzammyl <muzammil_75@outlook.com>
2021-12-31 12:53:21 +0500

    added String* in dataType*

---
+++ src/dataTypes/stringType/stringDataType.java
@@ -0,0 +1,8 @@
+package src.dataTypes.stringType;
+
+class stringDataType {
+    public static void main(String[] args) {
+        String str = "This is simply a String value, Text.";
+        System.out.println(str);
+    }
+}
