eb33b44602955ec5bbb5e3d4bb29d9395887c618
muzammyl <muzammyl@outlook.com>
2022-07-17 23:47:19 +0500

    added array.length*

---
+++ src/arrays/array_length.java
@@ -0,0 +1,12 @@
+package src.arrays;
+
+class array_length {
+    public static void main(String[] args) {
+        int a1[] = new int[12];
+        int a2[] = {3, 6, -9, 1, 43, 21, 76};
+        int a3[] = {1, 2, 3};
+        System.out.println("length of a1: " + a1.length);
+        System.out.println("length of a2: " + a2.length);
+        System.out.println("length of a3: " + a3.length);
+    }
+}
