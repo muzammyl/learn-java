61392256cf2421600cec0b674e43b4a984af42e9
muzammyl <muzammyl@outlook.com>
2022-07-22 23:45:29 +0500

    fixed

--- src/arrays/varArgs.java
+++ src/arrays/varArgs.java
@@ -14,3 +14,32 @@ class varArgs {
         vatest("arg3");
     }
 }
+
+class overloadVarargs {
+    static void test(int ... v) {
+        System.out.print("Number of args: " + v.length + " Contents: ");
+        for(int x: v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+    static void test(Boolean ... v) {
+        System.out.print("Number of args: " + v.length + " Contents: ");
+        for(boolean x: v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+    static void test(String msg, int ... v) {
+        System.out.print(msg + v.length + " Contents: ");
+        for(int x: v) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        test(4, 3, 0);
+        test(false, true, false, true);
+        test("Number of args: ", 83, 21);
+    }
+}
