4be496e127e7b09306ad6f4d0e74b0340bda262b
muzammyl <muzammyl@outlook.com>
2022-08-09 23:43:03 +0500

    added import*

---
+++ src/packages/p2/importDemo.java
@@ -0,0 +1,9 @@
+package src.packages.p2;
+
+import src.packages.p1.protection;
+
+class importDemo {
+    public static void main(String[] args) {
+        protection obj = new protection();
+    }
+}
